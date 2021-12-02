0f0c75c9b779886386ef72bf306a7caebd6414e7
Mohammad Jaha <miro.tune@gmail.com>
2021-12-02 05:55:01 +0000

    Uploading Ass

--- ColdCall2/ViewController.swift
+++ ColdCall2/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let names = ["Miro","Mohammad","Reem","Lubabah","Shahad","Keven","Almin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func coldCall(_ sender: Any) {
+        nameLabel.text = names[Int.random(in: 0..<names.count)]
+        
+        let number = Int.random(in: 1...5)
+        numberLabel.text = String(number)
+        numberLabel.isHidden = false
+        
+        if number == 1 || number == 2{
+            numberLabel.textColor = .red
+        }
+        else if number == 3 || number == 4{
+            numberLabel.textColor = .orange
+        }
+        else{
+            numberLabel.textColor = .green
+        }
+    }
+    
 }
 
